e8be53f45af6a0111eeb6f20776c77e130647fa0
Willochs Ojigbo <82782869+Willochs316@users.noreply.github.com>
2022-11-12 09:20:09 +0100

    updated fizzbuzz

--- 0-fizzbuzz.py
+++ 0-fizzbuzz.py
@@ -27,7 +27,7 @@ def fizzbuzz(n):
     print(" ".join(tmp_result))
 
 
-if __name__ == '__main__':
+if _name_ == '_main_':
     if len(sys.argv) <= 1:
         print("Missing number")
         print("Usage: ./0-fizzbuzz.py <number>")
